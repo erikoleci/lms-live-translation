4dab2b238399ee36c41657a2b6d67763253f0eab
Endri-cpu <endrihoxhaj380@gmail.com>
2026-08-25 16:41:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,12 @@ export default defineConfig({
     vuetify({ autoImport: true }),
   ],
   server: {
-    host: '0.0.0.0',
     port: 5000,
-    allowedHosts: true,
-  },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  }
 })
